e7801e140d640f4b23bcbf7a8b12367e1f8e7dc2
Christina Kothapally <christinakoth03@gmail.com>
2026-03-03 19:25:32 +0000

    https://abu.awsaibot.com/webhook/candidate  update the webhook URL for t

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
     // Differentiate based on the payload structure
     if (body.event === 'get-data-trigger') {
       // This is for the "Candidates" page
-      webhookUrl = 'https://votim.app.n8n.cloud/webhook-test/candidate';
+      webhookUrl = 'https://abu.awsaibot.com/webhook/candidate';
     } else if (body.event === 'get-interview-data-trigger') {
         // This is for the "AI Interviews" page
         webhookUrl = 'https://votim.app.n8n.cloud/webhook-test/candidateresult';
