8d2d102f58bb37c8b6fed2fc5d77c5b906b1c48c
Christina Kothapally <christinakoth03@gmail.com>
2026-03-03 14:23:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-997645626-bdae4",
+  "appId": "1:653702813766:web:6b3be4dc5c1cf0a26d5247",
+  "apiKey": "AIzaSyBLx_TkVsgkS7tn1Iumw12kOfdIX01m9VY",
+  "authDomain": "studio-997645626-bdae4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "653702813766"
+};
