c58f3e57226525abc12550c2c79cb8a6af061140
Christina Kothapally <christinakoth03@gmail.com>
2026-03-14 10:21:32 +0000

    Try fixing this error: `Console Error: Request to webhook destination fa

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
     // Differentiate based on the payload structure
     if (body.event === 'get-data-trigger') {
       // This is for the "Candidates" page
-      webhookUrl = 'https://abu.awsaibot.com/webhook/candidate';
+      webhookUrl = 'https://abu.awsaibot.com/webhook/candidateresult';
     } else if (body.event === 'get-interview-data-trigger') {
         // This is for the "AI Interviews" page
         webhookUrl = 'https://abu.awsaibot.com/webhook/candidateresult';
