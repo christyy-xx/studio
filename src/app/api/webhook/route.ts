a79669d7b56f322e4403b74cf48bc3ce5c495b03
Christina Kothapally <christinakoth03@gmail.com>
2026-03-14 10:58:21 +0000

    https://abu.awsaibot.com/webhook-test/contracts this is the url

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: Request) {
         webhookUrl = 'https://abu.awsaibot.com/webhook-test/contracts';
     } else {
       // This is for the "Job Posting" page
-      webhookUrl = 'https://abu.awsaibot.com/webhook/firebase-job-post';
+      webhookUrl = 'https://abu.awsaibot.com/webhook-test/contracts';
     }
 
     const response = await fetch(webhookUrl, {
